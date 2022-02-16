b4ad41df8adaf74d373e60da350e92f6318c9633
DESKTOP-VI6DH46\phaju <phajun105@naver.com>
2022-02-16 18:21:57 +0900

    원근감 추가

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ canvas.pack()
 canvas.create_rectangle(0, 300, 800, 600, fill = "green")
 
 BORD_COL = ["white", "silver", "gray", "azure4", "gray30", "gray20", "gray10", "black"]
-for i in range(1, 25):
-    w = i * 33
-    h = 12
-    x = 400 - w / 2
-    y = 288 + i * h
 
-    col = BORD_COL[i % 8]
+h = 2
+y = 300
+
+for i in range(1, 25):
+    w = i * i * 1.5
+    x = 400 - w / 2
+    
+    col = BORD_COL[i % len(BORD_COL)]
     canvas.create_rectangle(x, y, x + w, y + h, fill = col)
+
+    y = y + h
+    h = h + 1
 
 root.mainloop()
